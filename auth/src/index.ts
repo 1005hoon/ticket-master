fbad99dbfd915bb91b6396a7117f59e7f11cf56e
Hoon <shoh@help-me.kr>
2021-07-15 14:45:12 +0900

    add setup for ingress nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,4 +4,12 @@ const app = express()
 
 app.use(express.json({}))
 
+app.get('/api/v1/users/currentUser', (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: "User",
+    error: null
+  })
+})
+
 app.listen(3000, () => console.log("Server running on port 3000"))
